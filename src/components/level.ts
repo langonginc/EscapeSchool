38237a8e33a6f6a4f435904742aac20b03b547e3
5+1 <59787082+langonginc@users.noreply.github.com>
2026-03-31 22:15:56 +0800

    level

--- src/components/level.ts
+++ src/components/level.ts
@@ -4,7 +4,7 @@ import { Level1Scene } from './levels/level1';
 export const LEVEL_COUNTS = 5;
 
 export const setupGameScenes = (game: ex.Engine) => {
-    game.add('menuScene', new ex.Scene());
+    game.addScene('menuScene', new ex.Scene());
 
-    game.add('level1', new Level1Scene());
+    game.addScene('level1', new Level1Scene());
 }
