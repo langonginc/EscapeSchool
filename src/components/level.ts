f8b25af89788781409603587a969a894c9c6ec12
5+1 <59787082+langonginc@users.noreply.github.com>
2026-03-31 22:24:11 +0800

    fix levels

--- src/components/level.ts
+++ src/components/level.ts
@@ -4,6 +4,7 @@ import { Level1Scene } from './levels/level1';
 export const LEVEL_COUNTS = 5;
 
 export const setupGameScenes = (game: ex.Engine) => {
+    console.log("打包后 Level1Scene 是什么鬼？", Level1Scene);
     game.addScene('menuScene', new ex.Scene());
 
     game.addScene('level1', new Level1Scene());
